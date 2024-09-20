d36b651f67399e1be62f1e46eb590a98baabc529
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-20 19:19:58 +0500

    changes

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Meals() {
-  const [loadedMeals,setLoadedMeals] = useState([])
+  const [loadedMeals, setLoadedMeals] = useState([]);
 
-   async function fetchMeals(){
-    const response = await fetch("http//localhost:3000/meals")
-    const meals = response.json()
-   }
+  async function fetchMeals() {
+    const response = await fetch("http//localhost:3000/meals");
+    const meals = response.json();
+    setLoadedMeals(meals);
+  }
+
+  fetchMeals()
   
-    
   return (
     <div>
       <ul id="meals">
-
+        {loadedMeals.map((meal) => (
+          <li key={meal.id}>{meal.name}</li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
