adeea992dd506707309252d0750b8710a2e0d2ec
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-20 19:15:35 +0500

    changes

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,21 +1,16 @@
 
 
 export default function Meals() {
-
-    async function fetchApi(url) {
-        try {
-          let response = await fetch("");
-          let data = await response.json();
-          console.log("Fetched data: ", data);
-        } catch (error) {
-          console.log("Error fetching data: ", error);
-        }
-      }
+   async function fetchMeals(){
+    const response = await fetch("http//localhost:3000/meals")
+    const meals = response.json()
+   }
+  
     
   return (
     <div>
       <ul id="meals">
-        
+
       </ul>
     </div>
   )
