aeeec3ac027006b0128c2c350e78b4311d93b50e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-20 18:17:33 +0500

    changes

---
+++ src/components/Haeder.jsx
@@ -0,0 +1,13 @@
+export default function Haeder() {
+  return (
+    <header id="main-hreader">
+      <div id="title">
+        <img src="" alt="" />
+        <h1>React Food</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  )
+}
