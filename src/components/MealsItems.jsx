bf5bedad0a0069c4c1196c39f761da874ea03d69
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-21 16:30:41 +0500

    changes

--- src/components/MealsItems.jsx
+++ src/components/MealsItems.jsx
@@ -9,7 +9,7 @@ export default function MealsItems({ meal }) {
           <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
           <div>
             <h3>{meal.name}</h3>
-            <p className="meal-item-price">{currencyFormatter(meal.price)}</p>
+            <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
             <p className="meal-item-description">{meal.description}</p>
           </div>
           <p className="meal-item-actions">
