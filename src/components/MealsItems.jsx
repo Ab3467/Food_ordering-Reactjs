87ef0b67e67b5ae7bc926fead54890a7b7bf3635
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-21 16:27:05 +0500

    changes

--- src/components/MealsItems.jsx
+++ src/components/MealsItems.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { currencyFormatter } from "../utils/formatting.js";
 
 export default function MealsItems({ meal }) {
   return (
@@ -8,7 +9,7 @@ export default function MealsItems({ meal }) {
           <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
           <div>
             <h3>{meal.name}</h3>
-            <p className="meal-item-price">{meal.price}</p>
+            <p className="meal-item-price">{currencyFormatter(meal.price)}</p>
             <p className="meal-item-description">{meal.description}</p>
           </div>
           <p className="meal-item-actions">
