287124bc66e72573ba066a5cae3169c3fcb6cd9a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 13:44:39 +0500

    changes

--- src/store/UserProgressContext.jsx
+++ src/store/UserProgressContext.jsx
@@ -1,16 +1,43 @@
-import { createContext } from "react"
+import { createContext, useState } from "react";
 
- export const UserProgressContext = createContext({
-  progress : '',
-  showCart : ()=> {},
-  hidecart : ()=> {},
-  showCheckout : ()=> {},
-  hideCheckout : ()=> {},
- });
+export const UserProgressContext = createContext({
+  progress: "",
+  showCart: () => {},
+  hidecart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
 
- export default function UserProgressContextProvider({children}){
-    return <UserProgressContext.Provider>
+export function UserProgressContextProvider({ children }) {
+  const [userProgress, setUserProgress] = useState("");
+
+  function showCart() {
+    setUserProgress("cart");
+  }
+
+  function hideCart() {
+    setUserProgress("");
+  }
+
+  function showCheckout() {
+    setUserProgress("checkout");
+  }
+
+  function hideCheckout() {
+    setUserProgress("");
+  }
+
+  const userProgressCtx = {
+    progress: userProgress,
+    showCart: showCart,
+    hideCart: hideCart,
+    hideCheckout: hideCheckout,
+    showCheckout: showCheckout,
+  };
+
+  return (
+    <UserProgressContext.Provider value={userProgressCtx}>
       {children}
     </UserProgressContext.Provider>
- }
-
+  );
+}
