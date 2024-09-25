cd00016c1bd12a47f7484c69b25d05d73bcd57bf
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 11:46:13 +0500

    changes

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -6,17 +6,33 @@ export const CartContext = createContext({
   removeIems: (id) => {},
 });
 
-function cartReducer(state, action){
-  if(action.type === 'ADD_ITEM'){
-    //...
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const updatedItems = [...state, existingCartIndex];
+
+    if (existingCartIndex < -1) {
+      const existingItems = state.items[existingCartIndex];
+      const updatedItem = {
+        ...existingItems,
+        quanitity: existingItems.quanitity + 1,
+      };
+      updatedItems[existingCartIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.items, quanitity: 1 });
+    }
+
+    return {};
   }
-  if(action.type === 'REMOVE_ITEM'){
-    // ...
+  if (action.type === "REMOVE_ITEM") {
   }
+  return state;
 }
 
-export default function CartContextProvider({children}){
-    useReducer();
-    
-    return <CartContext.Provider>{children}</CartContext.Provider>
+export default function CartContextProvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+
+  return <CartContext.Provider>{children}</CartContext.Provider>;
 }
