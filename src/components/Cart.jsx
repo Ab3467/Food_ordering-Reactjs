1cf7fdfaa5c50094756de1af624d28463e9fbc80
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 16:03:32 +0500

    changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -22,7 +22,7 @@ export default function Cart() {
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <CartItem key={item.key} {...item}/>
+          <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.price}/>
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
