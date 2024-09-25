bee1d9e71628851f3869b4fddf83fe06b5b4287b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 13:32:37 +0500

    changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import Modal from "./UI/Modal";
 import { useContext } from "react";
 import { CartContext } from "../store/CartContext";
 import { currencyFormatter } from "../utils/formatting";
+import Button from "./UI/Button";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -10,6 +11,9 @@ export default function Cart() {
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
+  const handleRemoveItem = ()=>{
+    
+  }
   return (
     <Modal className="cart">
       <h2>Your Cart</h2>
@@ -21,6 +25,10 @@ export default function Cart() {
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-action">
+        <Button>Close</Button>
+        <Button onClick={handleRemoveItem}>Checkout</Button>
+      </p>
     </Modal>
   );
 }
---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,9 @@
+
+
+export default function UserProgressContext() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
