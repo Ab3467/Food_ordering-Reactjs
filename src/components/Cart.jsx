e71d472763fef22c56f9010b39c668aed9f0f290
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 13:26:32 +0500

    changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,17 @@
 import Modal from "./UI/Modal";
 import { useContext } from "react";
 import { CartContext } from "../store/CartContext";
+import { currencyFormatter } from "../utils/formatting";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
   return (
-    <Modal>
+    <Modal className="cart">
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
@@ -14,6 +20,7 @@ export default function Cart() {
           </li>
         ))}
       </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
     </Modal>
   );
 }
