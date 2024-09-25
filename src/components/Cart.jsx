cd503b6006a4ab0af27c48f268ae327af4b890a3
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 13:07:16 +0500

    changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,19 @@
+import Modal from "./UI/Modal";
+import { useContext } from "react";
+import { CartContext } from "../store/CartContext";
 
 export default function Cart() {
+  const cartCtx = useContext(CartContext);
   return (
-    <div>
-      
-    </div>
-  )
+    <Modal>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </Modal>
+  );
 }
