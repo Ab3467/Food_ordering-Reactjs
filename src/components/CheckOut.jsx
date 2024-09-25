2e730f69e7e2dc866e4aed06056e3655c1f352a7
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 18:14:15 +0500

    changes

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Modal from "./UI/Modal";
 import { CartContext } from "../store/CartContext";
 import { currencyFormatter } from "../utils/formatting";
+import Input from "./UI/Input";
 
 export default function CheckOut() {
   const cartCtx = useContext(CartContext);
@@ -14,7 +15,12 @@ export default function CheckOut() {
       <form action="">
         <h2>Checkout </h2>
         <p>Total amount: {currencyFormatter.format(cartTotal)}</p>
-        
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Email Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" />
+        </div>
       </form>
     </Modal>
   );
