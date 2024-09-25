8c36e5e141edb71838603a959dbcd409bf1a3f68
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 12:17:19 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import "./index.css";
+import CartContextProvider from "./store/CartContext";
+import { useContext } from "react";
+import { CartContext } from "./store/CartContext";
 
 function App() {
   return (
