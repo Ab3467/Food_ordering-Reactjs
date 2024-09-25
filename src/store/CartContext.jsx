c3bbdf27d8142ed08cf0a99ca342a37d0c49323e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-25 11:11:01 +0500

    changes

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,8 +1,7 @@
-
 import { createContext } from "react";
 
 const CartContext = createContext({
-    items:[],
-    addItems : (item) => {},
-    removeIems : (item) => {}
-})
+  items: [],
+  addItems: (item) => {},
+  removeIems: (item) => {},
+});
