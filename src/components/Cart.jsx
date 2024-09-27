daeb7b0a4ddc307ecf493d085b8e79446dff118f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-27 16:33:37 +0500

    changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -21,7 +21,7 @@ export default function Cart() {
     userProgCtx.showCheckout();
   }
   return (
-    <Modal className="cart" open={userProgCtx.progress == "cart"} onClose={handleCloseCart}>
+    <Modal className="cart" open={userProgCtx.progress == "cart"} onClose={userProgCtx.progress == 'cart' ? handleCloseCart : null}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
