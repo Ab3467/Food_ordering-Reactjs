fbde5eb8bc9e98b72543cf059992cacef4d0c02a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-09-28 11:53:22 +0500

    changes

--- src/hooks/UseHttp.jsx
+++ src/hooks/UseHttp.jsx
@@ -1,11 +1,40 @@
+import { useEffect } from "react";
+import { useState, useCallback } from "react";
+
 async function sendHttpsRequest(url, confing) {
-  const response = await fetch(url);
+  const response = await fetch(url, confing);
+
+  const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error("something went wrong");
+    throw new Error(resData.message || "something went wrong");
   }
+
+  return resData;
 }
 
 export default function UseHttp() {
-  return <div></div>;
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const sendRequest = useCallback(async function sendRequest() {
+    setIsLoading(true);
+    try {
+      const resData = sendHttpsRequest();
+      setData(resData);
+    } catch (error) {
+      setError(error.message || "something went wrong");
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest,
+  };
 }
